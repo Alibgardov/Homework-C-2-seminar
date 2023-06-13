0e5644ba9412b72657bf6a2f69737520fca0c9ab
Andrew <alibgardov@mail.ru>
2023-06-13 15:59:35 +0400

    Task10 done

---
+++ Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int Prompt (string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+int number = Prompt("Введите трёхзначное число > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трёхзначное число, пожалуйста повторите снова");
+    return;
+}
+Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
